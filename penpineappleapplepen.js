f6ee31734fe35f13dcaec96c3026c19fe332ceab
iria <neveiscool1@gmail.com>
2025-12-23 21:30:35 +0800

    they were useless

--- penpineappleapplepen.js
+++ penpineappleapplepen.js
@@ -1,9 +1,6 @@
 // i ate dinner at 6 pm, stayed at my kitchen with my roommate for 
 // hmm 3 hours and i lowkey dont want to do this anymore sobs
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
 document.addEventListener("mousemove", function(banana) {
     const stuff = document.createElement("div");
     stuff.className = "trail";
