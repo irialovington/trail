c445c2c8eb2da80f45859cad7dc8afecb5208dc4
iria <neveiscool1@gmail.com>
2025-12-23 21:07:13 +0800

    js

---
+++ penpineappleapplepen.js
@@ -0,0 +1,5 @@
+// i ate dinner at 6 pm, stayed at my kitchen with my roommate for 
+// hmm 3 hours and i lowkey dont want to do this anymore sobs
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
